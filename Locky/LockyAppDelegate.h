d8ef8a4db7bc95a4a19836a1f8e5f17dd91baf4c
Julien Oster <julien.oster@jambit.com>
2013-09-19 13:45:04 +0200

    Initial Commit

---
+++ Locky/LockyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LockyAppDelegate.h
+//  Locky
+//
+//  Created by Julien Oster on 19.09.13.
+//  Copyright (c) 2013 Julien Oster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LockyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Locky/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Locky
+//
+//  Created by Julien Oster on 19.09.13.
+//  Copyright (c) 2013 Julien Oster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LockyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LockyAppDelegate class]));
+    }
+}
